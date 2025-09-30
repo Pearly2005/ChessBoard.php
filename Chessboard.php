f07a2919f4b8efd5bb1cb6af0c861c47cc7423d6
Pearly2005 <borlazapearlmarie@smcbi.edu.ph>
2025-09-30 11:42:48 +0800

    Update Chessboard.php

--- Chessboard.php
+++ Chessboard.php
@@ -2,22 +2,48 @@
 <html>
 <head>
     <title>Chessboard in PHP</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        td {
+            text-align: center;
+            vertical-align: middle;
+            font-size: 24px;
+            font-family: "Segoe UI Symbol", "Arial Unicode MS", sans-serif;
+        }
+    </style>
 </head>
 <body>
 
 <?php
-echo "<table width='270px' cellspacing='0' cellpadding='0' border='1'>";
+// Unicode chess pieces
+$pieces = [
+    "black" => ["♜","♞","♝","♛","♚","♝","♞","♜"],
+    "white" => ["♖","♘","♗","♕","♔","♗","♘","♖"]
+];
 
-for ($row = 1; $row <= 9; $row++) {   
+echo "<table width='240px' cellspacing='0' cellpadding='0' border='1'>";
+
+for ($row = 1; $row <= 8; $row++) {
     echo "<tr>";
-    for ($col = 1; $col <= 9; $col++) {
-      
-        if (($row + $col) % 2 == 0) {
-            $color = "#FFFFFF"; 
-        } else {
-            $color = "#000000";  
-        }
-        echo "<td height='30px' width='30px' bgcolor='$color'></td>";
+    for ($col = 1; $col <= 8; $col++) {
+        // Alternate square colors
+        $color = (($row + $col) % 2 == 0) ? "#FFFFFF" : "#000000";
+        $piece = "";
+
+        // Place black pieces
+        if ($row == 1) $piece = $pieces["black"][$col - 1];
+        if ($row == 2) $piece = "♟";
+
+        // Place white pieces
+        if ($row == 7) $piece = "♙";
+        if ($row == 8) $piece = $pieces["white"][$col - 1];
+
+        // Set text color so pieces are visible
+        $textColor = ($color == "#000000") ? "white" : "black";
+
+        echo "<td height='30px' width='30px' bgcolor='$color' style='color:$textColor;'>$piece</td>";
     }
     echo "</tr>";
 }
@@ -27,3 +53,4 @@ echo "</table>";
 
 </body>
 </html>
+
